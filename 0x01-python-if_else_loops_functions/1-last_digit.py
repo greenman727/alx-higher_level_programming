9a213f5d3f2a4f34ed887169797b3ff07a1390c1
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-08 16:12:31 -0700

    created a file to print alphabet

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3 
-import random 
+#!/usr/bin/python3
+import random
 number = random.randint(-10000, 10000)
 lastdigit = abs(number) % 10
 
@@ -8,4 +8,5 @@ if number > 5:
 elif number == 0:
     print(f"Last digit of {number} is {lastdigit} and is 0")
 else:
-    print(f"Last digit of {number} is -{lastdigit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is -{lastdigit} and is less than 6\
+            and not 0")
