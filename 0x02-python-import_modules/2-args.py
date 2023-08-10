df8a1cb0048283f6b0487a06d0b465e94d19bae7
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-10 11:54:04 -0700

    created a file to print a variable from a file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def argument(*n, **args):
+    for i in arguments:
+        if i == 1:
+            print("argument")
+        else:
+            print("arguments")
+        print("{} {}:".format(len(argv), i)))
+        print(
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from variable_load_5.py import a
+
+if __name == "__main__":
+print(a)
