c81350e5cb0faee2c4a69428fc2ec415ed0a258c
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-09 23:30:57 -0700

    created a file to impoet add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,8 @@
 from add_0 import add
 a = 1
 b = 2
-c = add(a, b)
 print("{}".format(a))
 print("{}".format(b))
-print("{} + {} = {}".format(a, b, c))
+print("{} + {} = {}".format(a, b, (a + b)))
 
 add(a, b)
