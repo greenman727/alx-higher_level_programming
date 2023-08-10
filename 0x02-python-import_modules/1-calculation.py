4248514257be382a0609041f81eb6ed2c706e8ef
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-09 23:26:59 -0700

    created a file to import calculator function

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, (a + b))
+print("{} + {} = {}".format(a, b, (a - b))
+print("{} + {} = {}".format(a, b, (a * b))
+print("{} + {} = {}".format(a, b, (a / b))
+add(a, b)
+sub(a, b)
+mul(a, b)
+div(a, b)
