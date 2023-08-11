6948f005429f3ee8c5ec057e48ae195a9c7bb2e8
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-11 00:31:58 -0700

    created a file to import calculator file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,10 +4,6 @@ if __name_- == __import__:
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b,sub(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, (div(a, b)))
-    add(a, b)
-    sub(a, b)
-    mul(a, b)
-    div(a, b)
+    print("{} / {} = {}".format(a, b, div(a, b)))
