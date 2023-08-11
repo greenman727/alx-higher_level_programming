2c728ecfbf0166c52c1ece077001c54f40ed6f54
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-11 08:52:17 -0700

    created a file to print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    def argument(*args):
-        for i in args:
-            if i == 1:
-                print("{} argument".format(len(argv))
+        import sys
+            ar = len(sys.argv) - 1
+            if ar == 0:
+                print("0 arguments.")
+            elif ar == 1:
+                print("1 argument.")
             else:
-                print("{} arguments".format(len(argv))
+                print("{} arguments:".format(i))
+            for x in range(ar):
+                print("{}: {}."format(ar + 1, sys(argv[ar + 1]))
+            
