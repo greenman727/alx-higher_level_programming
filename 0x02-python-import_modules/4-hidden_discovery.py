32d2215014e90485aa193489edd2d3b3187fd5f1
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-11 00:40:33 -0700

    created a file to print names of funtions

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
+    import hidden_4
     names = dir(hidden_4)
-    sorted_names(name for name in names if not name.startswitch("__"))
-    for name in sorted_names:
-        print(name)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
+        else:
+            return name
