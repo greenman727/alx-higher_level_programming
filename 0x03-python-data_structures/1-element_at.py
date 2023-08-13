0695eee4e2501df0db4a99c7a55335a2a64ce8f5
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-12 17:00:16 -0700

    created a module to print a number in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return none
+        return None
     elif idx > len(my_list):
-        return none
+        return None
     else:
-        print(idx)
+        print(my_list[idx])
