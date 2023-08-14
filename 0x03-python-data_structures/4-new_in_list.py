f3e7aabe9b22e90ee0217111614b453446c892c8
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-14 15:37:10 -0700

    created a module to print a copy of a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    copy = my_list.copy()
     if idx < 0:
-        return my_list
-    elif idx > (len(my_list) - 1):
-        return my_list
+        return my_list.copy()
+    elif idx > len(my_list) - 1:
+        return my_list.copy()
     else:
-        return my_list.pop(idx)
+        copy[idx] = element
+        return copy
