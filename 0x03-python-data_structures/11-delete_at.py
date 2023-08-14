23d17f54529d7b8baf5fb297217fb9be65a7d4b3
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-14 13:23:10 -0700

    created a module to delete a string in a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,6 +2,6 @@
 def delete_at(my_list=[], idx=0):
     for i in my_list[:]:
         if idx < 0 or idx > (len(my_list) - 1):
-            return my_list
+            return my_list[:]
         else:
             del my_list[idx]
