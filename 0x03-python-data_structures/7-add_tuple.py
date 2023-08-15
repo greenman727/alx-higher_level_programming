7cd47dc38d57dc8d6a125c7bbdfe94c109c7209f
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-15 00:26:26 -0700

    created a module to add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
     if len(tuple_a) < 2:
-        tuple_x = tuple_a + (0, 0)
+        tuple_a = tuple_a(0, 0)
     if len(tuple_b) > 2:
-        tuple_y = tuple_b + (0, 0)
-        new_tuple = tuple_x[0] + tuple_y[0]), (tuple_x[1 + tuple_y[1]
+        tuple_b = tuple_b((tuple_b[0], tuple_b[1])
+    new_tuple = tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]
     return new_tuple
