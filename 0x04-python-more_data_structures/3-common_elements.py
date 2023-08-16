ddc23981c641153394b5430f10fb912be744b4ab
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-16 13:31:37 -0700

    created a module to find common element

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if (set_1 & set_2):
-        return (set_1 & set_2)
-    else:
-        return None
+    if len(set_1.intersection(set_2)) > 0:
+        return (set_1.intersection(set_2))
