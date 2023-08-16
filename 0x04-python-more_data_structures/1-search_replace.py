16ec063ad1380fd78a946e1981d564e48740cedb
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-16 08:51:11 -0700

    created a module to add tuple

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for item in my_list:
-            if my_list.index[item] == search:
-                my_list.index[item] = replace
+            if my_list[item] == search:
+                my_list[item] = replace
