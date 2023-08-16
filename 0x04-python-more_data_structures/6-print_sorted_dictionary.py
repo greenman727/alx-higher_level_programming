8b55bf11f1849c6424092535a142a0468c710191
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-16 14:51:46 -0700

    created a module to print keys in alpha order

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.keys()):
-        return key
-
+    return [for key in sorted(a_dictionary.keys()):
