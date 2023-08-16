2b3aeab35553df578ee84cca5f67d47d6e6046b9
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-16 08:07:11 -0700

    created a module to replace item in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list = []
     for idx, search in my_list.index[search in enumerate(my_list):
             if mylist[idx] == search:
                 my_list[idx] = replace
