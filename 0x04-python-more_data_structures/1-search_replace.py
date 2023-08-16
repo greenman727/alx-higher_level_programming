c0f9463bbf2775d1275f44652ca66a02c2bd5ba4
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-16 15:12:01 -0700

    created an item to replace an element in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for item in range(len(my_list)):
-        if my_list[item] == search:
-            my_list[item] = replace
+        if item == search:
+            new_list,append(replace)
+        else:
+            new_list.append(item)
+    return new_list
