64be28be47b7649e5557a4e0e29667b181c3b646
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-16 15:32:54 -0700

    created a module to matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for col in matrix:
-        answer = list(map(lambida a: a**2, col))
+        answer = list(map(lambda a: a**2, col))
         new_matrix.append(answer)
     return new_matrix
