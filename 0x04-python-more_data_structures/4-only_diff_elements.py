3ecae8c7d2511ba49eed07e64185062bb497e7d6
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-16 14:08:30 -0700

    created a module t find a number not in each of the list

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return list(set(set_1).difference(set_2)) and list(set(set_2).differnce(set_1))
+    for item in set_1:
+        if item not in set_2:
+            return item
+    for item in set_2:
+        if item not in set_1:
+            return item
