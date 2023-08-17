8a88fb9881ba581c335bf3bf78f6bfca76e35c2f
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-17 09:19:35 -0700

    created a module to delete a key in a dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for key in a_dictionary:
-        if key in a_dictionary:
-            return del dictionary["key"]
-        else:
-            return None
+    if key in a_dictionary:
+        return del dictionary["key"]
+    else:
+        return None
