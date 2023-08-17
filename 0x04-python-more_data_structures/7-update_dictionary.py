602281723f0c97c1f5169e915d5bff7aa5952a08
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-17 09:05:14 -0700

    created a module to add value to key or value to a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key not in a_dictionary.keys():
+    if "key" not in a_dictionary.keys():
         a_dictionary["key"] = value
     if value not in a_dictionary.values():
         a_dictionary["key"] = value
