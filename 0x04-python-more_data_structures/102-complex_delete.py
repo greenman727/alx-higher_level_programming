57f1f4255d66b6393c9b3104f485866466a0a35c
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-18 00:31:31 -0700

    created a module to delete keys with certain values

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,4 @@
 def complex_delete(a_dictionary, value):
-    for key in list(a_dictionary.keys()):
+    for key, value in a_dictionary.keys():
         if a_dictionary[key] == value:
             del a_dictionary[key]
