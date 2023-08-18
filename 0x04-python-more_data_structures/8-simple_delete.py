ef835651b31435a0b2f21abe5e8435a518155da9
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-18 02:26:10 -0700

    created a module to find common items in a set

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        a_dictionary.update(del a_dictionary[key])
+        a_dictionary.update(a_dictionary.pop("key", None)
