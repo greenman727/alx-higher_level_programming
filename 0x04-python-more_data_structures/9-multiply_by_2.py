4d09740907881e3b36878a5f006b0dde1df8639b
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-17 23:17:14 -0700

    created a module to multiply values by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dictionary = []
-    for value in a_dictionary.values():
-        return (value * 2)
+    for key in a_dictionary:
+        a_dictionary[key] *= 2
     return new_dictionary
