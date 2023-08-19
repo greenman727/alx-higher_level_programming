5326f7fb93442eb70fb653249acbf64a4c057c04
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-18 21:12:30 -0700

    created a module to find a key with the highest value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        my_dict = list(a_dictionary.keys())
+        score = 0
+        biggest = ""
+        for i in my_list:
+            if a _dictionary[i] > score:
+                score = a_dictionary[i]
+                biggest = i
+        return biggest
+
