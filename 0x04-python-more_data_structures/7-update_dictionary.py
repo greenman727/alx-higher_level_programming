bbe38152b7b1cacdf0288041f64b3422e9e4bd5b
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-18 20:55:25 -0700

    created a module to print add or replace a key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for key, value in a_dicitionary.items():
-        if "key" not in a_dictionary.keys():
-            a_dictionary["key"] = value
-        if value not in a_dictionary.values():
-            a_dictionary["key"] = value
+    a_dictionary[key] = value
+    return a_dictionary
