c042067659672cfde5466d09c1cd6a732922b947
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-28 16:34:38 -0700

    created a module to divide two integers

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        division = a / b
+        div_result = a / b
     except (ZeroDivisionError, TypeError):
-        division = None
+        div_result = None
     finally:
-        print("Inside result: {}".format(division))
-    return (division)
+        print("Inside result: {}".format(div_result))
+    return (div_result)
 
