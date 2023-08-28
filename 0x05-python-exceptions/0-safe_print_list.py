792347e756e2bb0917a6d7f13b71d8d6475dad46
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-28 04:37:22 -0700

    created module to print number of element in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        count = 0
         for x in my_list:
             count = count+1
-    except ValueError:
+    except IndexError:
         pass
     print(count)
     return count
