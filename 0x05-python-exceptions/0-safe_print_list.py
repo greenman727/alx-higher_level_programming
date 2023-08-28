b13bcf09b57de826700a8b509ac4f30e78f5b2d9
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-28 05:49:59 -0700

    created a module to print elements in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        print(my_list[:x])
+        my_list = []
+        for x in my_list:
+            if type(x) == int or type(x) == str:
+                my_list.append(x)
+            print(my_list)
     except IndexError:
         pass
-    return my_list[:]
+    return my_list
