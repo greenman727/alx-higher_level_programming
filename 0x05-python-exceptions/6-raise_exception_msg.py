4c4da058bb56656d34eb1ea7fab22a55f2397431
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-28 06:21:11 -0700

    created a module to raise exception

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     try:
-        raise Exception("message")
+        raise Exception(message)
     except Exception as error:
         print("error")
