c3ffd853adb14747ff0c024c300eea2d6853e2bf
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-28 15:27:11 -0700

    created a module to print stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError) as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return False
     else:
