41ba936e941aa1bcb071b53cf1767863fef9edf5
¨greenman727¨ <cornerx3m@gmail.com>
2023-08-29 15:03:38 -0700

    created a class that defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,6 +3,7 @@
 """define a class square"""
 
 class square:
+    """Represents a square Attributes."""
     def __init__(self, size):
         """Initialize a square
         Args:size (int): size of the side of the square.
