d9dbddb748cb4a9bdff136ca0a1dedb0505fc7b3
¨greenman727¨ <cornerx3m@gmail.com>
2023-09-03 01:13:58 -0700

    created a module to multiply two matrices

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module to multiply 2 matrices using the module NumPy"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """A function to return the multiplication of two matrices"""
+    return (np.matmul(m_a, m-b))
