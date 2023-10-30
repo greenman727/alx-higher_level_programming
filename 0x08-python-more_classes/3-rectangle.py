9774811ed07798205738b8fdbb346fc7205aef32
greenman727 <cornerx3m@gmail.com>
2023-10-30 13:12:06 -0700

    created a class Rectangle that defines a rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,6 +55,5 @@ class Rectangle:
             return("")
 
         if self___width != 0 and self.__height != 0:
-            string += "\n".join("#" * self__width 
-                    for j in range(self.__height))
+            string += "\n".join("#" * self__width for j in range(self.__height))
             return string
