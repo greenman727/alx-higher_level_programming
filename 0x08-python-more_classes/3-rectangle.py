910a36885fb9b9912a803087a047129ace004407
greenman727 <cornerx3m@gmail.com>
2023-10-30 13:26:00 -0700

    created a class Rectangle that defines a rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
 
     @property
     def width(self):
-        """set the width of the resctangle"""
+        """set the width of the rectangle"""
         return self.__width
 
     @width.setter
@@ -52,6 +52,5 @@ class Rectangle:
         """returns printable string representation of the rectangle"""
         string = ""
         if self___width != 0 and self.__height != 0:
-            string += "\n".join("#" * self__width \
-                    for j in range(self.__height))
+            string += "\n".join("#" * self__width for j in range(self.__height))
             return string
