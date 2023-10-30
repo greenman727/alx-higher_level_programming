4b500d669385b33ecd27732e85943e4fd8c9801b
greenman727 <cornerx3m@gmail.com>
2023-10-30 15:23:22 -0700

    created a class Rectangle that defines a rectangle

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """A class to define a Rectangle"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
