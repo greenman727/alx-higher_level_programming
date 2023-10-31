8c67dd7dfb4a5bed807e09a1c573058e86130db7
greenman727 <cornerx3m@gmail.com>
2023-10-30 23:13:34 -0700

    created a class Rectangle that defines a rectangle

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -58,8 +58,8 @@ class Rectangle:
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join(str(self.print_symbol) * self.__width
-                                            for j in range(self.__height))
-        return string
+                                                for j in range(self.__height))
+            return string
 
     def __repr__(self):
         """returns the string representation of the Rectangle"""
