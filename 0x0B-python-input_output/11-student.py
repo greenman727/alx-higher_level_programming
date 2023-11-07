b8c2482f809d610e8308ce03c65543bbd80c8b57
greenman727 <cornerx3m@gmail.com>
2023-11-07 11:24:56 -0800

    created a directory and README file to solve hash tables

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,17 +3,18 @@
 
 
 class Student:
-    """REpresentation of a student"""
+    """Representation of a student"""
+
     def __init__(self, first_name, last_name, age):
         """Initializes a student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Retrives a dictionary representation of a Student"""
-        if (typr(attrs) == list and
-                all(type(ele) == for ele in attrs)):
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
