9610ea67629c09bf0ed6affc15f9e66336f5470e
greenman727 <cornerx3m@gmail.com>
2023-11-07 12:11:36 -0800

    created classes for python input and output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,11 +9,10 @@ def pascal_triangle(n):
     
     triangles = [[1]]
     while len(triangles) != n:
-        tri = triangles[-1]
+        tria = triangles[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
+            tmp.append(tria[i] + tria[i + 1])
         tmp.append(1)
         triangles.append(tmp)
     return triangles
-
