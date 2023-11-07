9441720c364a58f422af73cc53a62aa59100a2ff
greenman727 <cornerx3m@gmail.com>
2023-11-07 10:44:49 -0800

    created a directory and README file to solve hash tables

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Defines a function that writes an Object to a text file"""
+import json
 
 
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file, using a JSON rrepresentation."""
     with open(filename, "w") as f:
-        json.dumpy(my_obj, f)
+        json.dump(my_obj, f)
