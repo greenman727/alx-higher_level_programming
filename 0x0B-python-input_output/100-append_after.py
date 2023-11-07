08afbf8e53cd2e5107509d962298979f0f1620c0
greenman727 <cornerx3m@gmail.com>
2023-11-07 09:04:37 -0800

    created classes for python input and output

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#1/usr/bin/python3
+"""Defines a function that inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
