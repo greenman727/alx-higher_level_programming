d62c6a6418fb0dd751aa461bb7a8801b6d9e8cd0
greenman727 <cornerx3m@gmail.com>
2023-11-07 07:28:26 -0800

    created a directory for python input and output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Writes a string to a UTFB text fille."""
-    with open(filename, "W", encoding="utf-8"i) as f:
+    with open(filename, "W", encoding="utf-8") as f:
         return f.write(text)
