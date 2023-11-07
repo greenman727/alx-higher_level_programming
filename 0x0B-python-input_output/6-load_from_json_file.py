b40a56402b64d01f923731b7f0a7bc001415a422
greenman727 <cornerx3m@gmail.com>
2023-11-07 10:47:20 -0800

    created a directory and README file to solve hash tables

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """Writes a function that creates an Object from a JSON file"""
-    with open(filename, "w") as f:
-        json.dumpy(my_obj, f)
+    with open(filename) as f:
+        return json.load(f)
